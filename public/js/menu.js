318ace9475de147057f80d7f4d87217ba4692594
karlosicolombia <karlosicolombia@protonmail.com>
2020-11-20 12:06:49 +0100

    menu.js

---
+++ public/js/menu.js
@@ -0,0 +1,21 @@
+var food = [
+  {
+  "name": "Ostburgare",
+  "kCal": 850,
+  "lactose": true,
+  "gluten": false,
+  "img": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2F2015%2F11%2Fgourmet-burger.jpg&f=1&nofb=1"
+},{
+"name": "Lövburgare",
+"kCal": 999,
+"lactose": false,
+"gluten": true,
+"img": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.thelastfoodblog.com%2Fwp-content%2Fuploads%2F2017%2F04%2FHalloumi-Burger-landscape.jpg&f=1&nofb=1"
+},{
+"name": "HYBRIDBURGER",
+"kCal": 9001,
+"lactose": true,
+"gluten": true,
+"img": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Finventorspot.com%2Ffiles%2Fimages%2Fstrange_food.preview.jpg&f=1&nofb=1"
+}
+]
